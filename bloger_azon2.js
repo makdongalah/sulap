bfc8960df5f32dc5641d94d945071edb471ae9a2
makdongalah <99934322+makdongalah@users.noreply.github.com>
2022-10-31 09:26:51 +0700

    Update bloger_azon2.js

--- bloger_azon2.js
+++ bloger_azon2.js
@@ -6,9 +6,9 @@ var link5 = 'ASIN2';
 var link6 = '-click2';
 var baru1 = '<div style="text-align:center;"><img src="http://ecx.images-amazon.com/images/I/';
 var baru2 = '_.jpg"/></center></div><br />';
-var baru3 = '<a href="https://movielot.us/" rel="nofollow" target="_blank">';
+var baru3 = '<a href="https://movieright.us/" rel="nofollow" target="_blank">';
 var baru4 = '<b>Check Latest Price</b></a>';
-var baru5 = '<a href="https://channelmovie.info//p/movie_playing/" rel="nofollow" target="_blank">';
+var baru5 = '<a href="https://movieright.us//p/movie_playing/" rel="nofollow" target="_blank">';
 var baru6 = '<div style="text-align:center;"><img width="170" height="70" border="0" src="https://4.bp.blogspot.com/-BUgsmdtI9qw/UBDsSp787kI/AAAAAAAAAEs/57EfkJtHvRw/s1600/8fCpt.gif"  /></center></a></div>';
 function updateme()
 {
